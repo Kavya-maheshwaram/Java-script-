46e8feece22d1ab51e9e522838e32aeeeea4e009
Kavya-maheshwaram <22r91a05d8@tkrec.ac.in>
2025-09-22 11:02:32 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,22 @@
+a = 7897;
+console.log(typeof a);
+Name ="kavya";
+console.log(typeof Name);
+C=true;
+console.log(typeof C);
+data = null;
+console.log(typeof data);
+D = undefined;
+console.log(typeof D);
+
+
+//non primitive
+array = [56, "kavya", true, null];
+console.log(typeof array);
+data = {
+    Name:"kavya",
+    Age:22,
+    Dept: "cse",
+};
+console.log(typeof data);
+
